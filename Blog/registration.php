f0c4f0cf9770597f5e06785c54b7084c60e3e9bf
ishiekh-dev <ishiekh.dev.v1@gmail.com>
2022-07-01 09:57:42 +0200

    add module registeration as start

---
+++ Blog/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * break up mageplaze module blog to figure it out 
+ * ishiekh.dev.v1@gmail.com
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Ishiekh_Blog',
+    __DIR__
+);
